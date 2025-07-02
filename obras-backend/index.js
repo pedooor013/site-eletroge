379523c860ac78214ecda29ca0b2ad1cd5ec897e
Pedro <pedrolopes.programador013@gmail.com>
2025-07-02 16:38:58 -0300

    conexao e testes realizados junto do MySQL, conexao e requisição de dados OK

--- obras-backend/index.js
+++ obras-backend/index.js
@@ -5,7 +5,7 @@ const app = express();
 const conexao = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'Limonadacomcanjica1507!',
+    password: '',
     database: 'eletrogeDB'
 });
 
@@ -21,4 +21,40 @@ app.get('/', (req, res) =>{
 });
 app.listen(3000, ()=>{
     console.log('Servidor rodando na porta 3000')
+});
+
+app.get('/obras/:id', (req, res) => {
+    const obraId = req.params.id;
+
+    const query = `
+        SELECT
+        o.id, o.nomeObra, o.descricao, o.imagem, o.progresso,
+        c.nomeServico AS categoria, c.icone
+        FROM obras o
+        LEFT JOIN conexaoobrascategorias oc ON o.id = oc.obra_id
+        LEFT JOIN categoriasobras c ON c.id = oc.categoriaObras_id
+        WHERE o.id = ?
+    `;
+    conexao.query(query, [obraId], (err, results) =>{
+        if(err){
+            console.error('Erro ao buscar obra: ' , err);
+            return res.status(500).send('Erro interno no servidor');
+        }
+        if(results.length === 0){
+            return res.status(404).send('Obra não encontrada!');
+        }
+
+        const obra ={
+            id: results[0].id,
+            nomeObra: results[0].nomeObra,
+            descricao: results[0].descricao,
+            imagem: results[0].imagem,
+            progresso: results[0].progresso,
+            categorias: results.map(row => ({
+                nome: row.categoria,
+                icone: row.icone
+            }))
+        };
+        res.json(obra);
+    });
 });
