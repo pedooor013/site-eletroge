93c666cf4d6015b4ce3e6b756465f0767601e2a7
Pedro Lopes <pedrolopes.programador013@gmail.com>
2025-11-26 17:10:05 -0300

    feat: inicio do desenvolvimento do models para cadastro de obras no server adm

--- server/models/adm.models.js
+++ server/models/adm.models.js
@@ -17,6 +17,58 @@ async function findUserByEmailModels(email){
     })
 }
 
+async function createNewWorkModels(newWork){
+    const {workName, workDescription, workProgress, workService, workImg} = newWork;
+    
+    pool.query(
+        `
+        INSERT INTO obras(nome, descricao, progresso)
+        VALUES($1, $2, $3);
+        `, [workName], [workDescription], [workProgress],
+        function(err){
+            if(err){
+                rej(err);
+            }else{
+                res({id: this.lastID, ...newWork})
+            }
+        }    
+    )
+    const workId = res.id;
+
+    await createNewWorkImagesModels(workId, workImg);
+    await createNewWorkServiceModels(workId, workService);
+
+    
+}
+
+async function createNewWorkImagesModels(workId, workImgs){
+    if(Array.isArray(workImgs) && workImgs.length > 0){
+        for(const imgs of workImgs){
+            pool.query(`
+                INSERT INTO imagens (url, obra_id, eh_principal)
+                VALUES($1, $2, $3);
+                `, [imgs.url, workId, imgs.principal || 0],
+                function(err){
+                    if(err){
+                        if(err){
+                            rej(err);
+                        }
+                    }
+                }
+            );
+        }
+    }
+}
+
+async function createNewWorkServiceModels(workId, workService){
+    if(Array.isArray(workService) && workService.length > 0){
+        for(const services of workService){
+            pool.query(`
+                INSERT INTO obra_servico()
+                `)
+        }
+    }
+}
 
 export default{
     findUserByEmailModels
