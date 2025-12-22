1231d278728a4f441e6e4bf8c3302697b39a48bb
Pedro Lopes <pedrolopes.programador013@gmail.com>
2025-12-21 22:40:19 -0300

    feat: ajuste nas rotas de API

--- public/adm/js/config/api.routes.js
+++ public/adm/js/config/api.routes.js
@@ -1,17 +1,14 @@
-// Configuração centralizada das rotas da API
-const API_BASE_URL = 'http://localhost:3000/eletroge';
-
 export const API_ROUTES = {
     // Login
-    LOGIN: `${API_BASE_URL}/login`,
+    LOGIN: `/login`,
     
     // Obras
-    WORKS: `${API_BASE_URL}/obras`,
-    WORK_DETAILS: (id) => `${API_BASE_URL}/obras/detalhes/${id}`,
-    CREATE_WORK: `${API_BASE_URL}/cadastrarObra`,
-    UPDATE_WORK: (id) => `${API_BASE_URL}/editarObra/${id}`,
-    DELETE_WORK: (id) => `${API_BASE_URL}/deletarObra/${id}`,
+    WORKS: `/obras`,
+    WORK_DETAILS: (id) => `/obras/detalhes/${id}`,
+    CREATE_WORK: `/cadastrarObra`,
+    UPDATE_WORK: (id) => `/editarObra/${id}`,
+    DELETE_WORK: (id) => `/deletarObra/${id}`,
     
     // Imagens
-    UPLOAD_IMAGES: `${API_BASE_URL}/upload`
+    UPLOAD_IMAGES: `/upload`
 };
